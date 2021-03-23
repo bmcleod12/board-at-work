6b4b1d1f30286a01c033be9b6fd2f2569a87b6be
Brenna McLeod <brenna.mcleod94@gmail.com>
2021-03-22 22:06:02 -0500

    wrote code to get data from announcements db and display it on the index.handlebars page

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO Announcements (announcement, createdAt, updatedAt) VALUES ("Work meeting rescheduled to Wednesday at 8am", "2021-02-21 10:53:45", "2021-02-21 10:53:45");
+INSERT INTO Announcements (announcement, createdAt, updatedAt) VALUES ("Work meeting rescheduled to Wednesday at 10am", "2021-02-21 10:53:45", "2021-02-21 10:53:45");
+
+INSERT INTO Birthdays (birthdayMonth, employee, createdAt, updatedAt) VALUES ("March", "Joe", "2021-02-21 10:53:45", "2021-02-21 10:53:45");
+
+INSERT INTO Events (event, date, createdAt, updatedAt) VALUES ("Team Builder", "2021-03-22", "2021-02-21 10:53:45", "2021-02-21 10:53:45");
+
+INSERT INTO Quotes (quote, createdAt, updatedAt) VALUES ("When you reach the end of your rope, tie a knot and hang on", "2021-02-21 10:53:45", "2021-02-21 10:53:45");
+
+INSERT INTO Reminders (reminder, createdAt, updatedAt) VALUES ("Quarter end responsibilities due 3/31/2021", "2021-02-21 10:53:45", "2021-02-21 10:53:45");
+
+INSERT INTO Roles (roles, createdAt, updatedAt) VALUES ("Administrator", "2021-02-21 10:53:45", "2021-02-21 10:53:45")
