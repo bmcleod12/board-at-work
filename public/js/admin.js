588c6a3e5004f188ccddc9e1369f2d9cf35e8d6a
KenzieAlyce <mackenziealyce@gmail.com>
2021-03-24 20:11:16 -0500

    corrected file paths, added dropdown

---
+++ public/js/admin.js
@@ -0,0 +1,8 @@
+document.getElementById("menu").addEventListener("click",menuSelect);
+
+function menuSelect () {
+//removeClass.hidden("selectedName"); - Display selected category
+
+//On submit button, send to handlebars ID?
+
+};
---
+++ test.js
@@ -0,0 +1,15 @@
+const source = document.getElementById('greet').innerHTML;
+
+const template = Handlebars.compile(source);
+ 
+const context = {
+  greeting: 'Hello World!'
+};
+
+ 
+const compiledHtml = template(context);
+
+const fill = document.getElementById('hello');
+
+fill.innerHTML 
+= compiledHtml;
