b827b1d411b5e7fded02d56acc0d1ba47796014b
KenzieAlyce <mackenziealyce@gmail.com>
2021-03-24 22:18:28 -0500

    test

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,13 +1,14 @@
 const dropdownButtons = document.querySelectorAll(".dropdownContent");
 
-dropdownButtons.forEach(element => element.addEventListener("click",menuSelect));
+dropdownButtons.forEach(element => {
+  return element.addEventListener("click", menuSelect);
+});
 
-function menuSelect (event) {
-var clickId = event.currentTarget.getAttribute("data-id");
-var category = document.querySelector("#" +clickId);
+function menuSelect(event) {
+  const clickId = event.currentTarget.getAttribute("data-id");
+  const category = document.querySelector("#" + clickId);
 
-    category.classList.remove("hidden");
+  category.classList.remove("hidden");
 
-console.log(clickId);
+  console.log(clickId);
 }
-
--- test.js
+++ test.js
@@ -1,15 +1,13 @@
-const source = document.getElementById('greet').innerHTML;
+const source = document.getElementById("greet").innerHTML;
 
 const template = Handlebars.compile(source);
- 
+
 const context = {
-  greeting: 'Hello World!'
+  greeting: "Hello World!"
 };
 
- 
 const compiledHtml = template(context);
 
-const fill = document.getElementById('hello');
+const fill = document.getElementById("hello");
 
-fill.innerHTML 
-= compiledHtml;
+fill.innerHTML = compiledHtml;
